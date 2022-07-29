12fb6579ddd0fd37d83c5f0d8244d50ed59b5281
Antonio González <58194284+AgongarDev@users.noreply.github.com>
2022-07-30 00:11:43 +0200

    entendiendo lo de los hooks

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const element = <h1> Hola qué haces? </h1>;
+const element = <h1> Hola qué haces? Por aquí</h1>;
 
 root.render(element);
