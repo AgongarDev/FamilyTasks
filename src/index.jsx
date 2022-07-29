6843a081cd28787d08c0d181229b70fcf194ebeb
Antonio González <58194284+AgongarDev@users.noreply.github.com>
2022-07-30 00:08:16 +0200

    hola

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const element = <h1> Hola qué haces? </h1>;
 
